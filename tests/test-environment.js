eb76d4634507731281e7ac2da0ea3b301f2db5d4
Marcelo Morgado <cmarcelom@gmail.com>
2020-06-01 21:02:35 +0100

    Making test run faster

--- tests/test-environment.js
+++ tests/test-environment.js
@@ -6,22 +6,32 @@ const Ganache = require("ganache-core");
 const NodeEnvironment = require("jest-environment-node");
 
 const startChain = async () => {
-  if (!process.env.PRIV_KEY || !process.env.MAINNET_NODE_URL) {
+  const { PRIV_KEY, MAINNET_NODE_URL } = process.env;
+
+  if (!PRIV_KEY || !MAINNET_NODE_URL) {
     throw Error("PRIV_KEY and MAINNET_NODE_URL not found in .env");
   }
-  const ganache = Ganache.provider({
-    fork: process.env.MAINNET_NODE_URL,
-    network_id: 1,
-    accounts: [
-      {
-        secretKey: process.env.PRIV_KEY,
-        balance: ethers.utils.hexlify(ethers.utils.parseEther("1000")),
-      },
-    ],
-  });
 
-  const provider = new ethers.providers.Web3Provider(ganache);
-  const wallet = new ethers.Wallet(process.env.PRIV_KEY, provider);
+  let provider;
+
+  // Note: Don't fork if using MoonNet node.
+  // See more: https://github.com/studydefi/money-legos/issues/58
+  if (MAINNET_NODE_URL.match(/node.moonnet/)) {
+    provider = new ethers.providers.JsonRpcProvider(MAINNET_NODE_URL);
+  } else {
+    const ganache = Ganache.provider({
+      fork: MAINNET_NODE_URL,
+      network_id: 1,
+      accounts: [
+        {
+          secretKey: PRIV_KEY,
+          balance: ethers.utils.hexlify(ethers.utils.parseEther("1000")),
+        },
+      ],
+    });
+    provider = new ethers.providers.Web3Provider(ganache);
+  }
+  const wallet = new ethers.Wallet(PRIV_KEY, provider);
 
   return { wallet, provider };
 };
@@ -47,10 +57,12 @@ class CustomEnvironment extends NodeEnvironment {
     this.provider = provider;
     this.global.wallet = wallet;
     this.global.provider = provider;
+    await this.provider.send("evm_snapshot", []);
   }
 
   async teardown() {
     await super.teardown();
+    await this.provider.send("evm_revert", ["0x1"]);
   }
 
   //@ts-ignore
